346fb94505cb7af992ebfb0b5a9de28a53b6c079
Jason Rostar <jason.rostar@yahoo.com>
2020-09-03 23:27:52 -0700

    added css grid for friends card layout

--- friends/src/components/FriendsCard.js
+++ friends/src/components/FriendsCard.js
@@ -1,17 +1,15 @@
 import React from 'react';
-
+import "./friendsCard.styles.css";
 
 const FriendsCard = props => {
 
   const { name, age } = props.friend;
 
   return(
-    <div >
+    <div className="friends_card">
         <h2>{name}</h2>
-        <div >
-          <h5>Age: {age}</h5>
-        </div>
-      </div>
+        <h5>Age: {age}</h5>
+    </div>
   );
 };
 
